641c4f0db7dafcc7de65d2cadad7a0de9879f6ee
trian damai <triannurizkillah@gmail.com>
2023-09-27 10:39:29 +0700

    initial

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,19 @@ plugins {
     id("com.android.application") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.0" apply false
     id("com.android.library") version "8.1.0" apply false
+    kotlin("jvm") version "1.8.0" apply false
+}
+buildscript {
+    dependencies{
+        classpath(kotlin("gradle-plugin", version = "1.8.0"))
+        classpath("com.github.dcendents:android-maven-gradle-plugin:2.1")
+    }
+}
+extensions.findByName("buildScan")?.withGroovyBuilder {
+    setProperty("termsOfServiceUrl", "https://gradle.com/terms-of-service")
+    setProperty("termsOfServiceAgree", "yes")
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
